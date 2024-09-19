403890e61b9aa8078eaa2d5b2ae2b90cea83f421
MahoganyGant <mahoganygant02@gmail.com>
2024-09-18 22:40:35 -0400

    adding superclass and circle subclass

---
+++ src/main/java/shapeProject/shapeProject.java
@@ -0,0 +1,11 @@
+package shapeProject;
+
+public class shapeProject {
+
+    public static void main(String[] args) {
+
+        Shape circle = new Circle(7);
+        circle.printArea();
+        circle.printPerimeter();
+    }
+}
